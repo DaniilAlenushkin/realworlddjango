4c73c44a7f85197d11bcb1ad2243749abc468fd5
Daniil Alenushkin <DaniilAlenushkin@yandex.ru>
2021-08-13 19:52:05 +0300

    Url Fix

--- events/urls.py
+++ events/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'events'
 
 urlpatterns = [
-    path('event_list/', views.EventListView.as_view(), name='event_list'),
-    path('event_detail/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('list/', views.EventListView.as_view(), name='event_list'),
+    path('detail/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
     path('event_update/<int:pk>/', views.EventUpdateView.as_view(), name='event_update'),
     path('event_delete/<int:pk>/', views.EventDeleteView.as_view(), name='event_delete'),
     path('event_create/', views.EventCreateView.as_view(), name='event_create'),
